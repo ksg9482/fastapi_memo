41daa5784117b562443c62b517f3a076db9b7fa4
ksg9482 <ksg930523@gmail.com>
2024-04-10 22:33:43 +0900

    Create: update memos

--- main.py
+++ main.py
@@ -52,6 +52,23 @@ async def list_memos(db:Session=Depends(get_db)):
     memos = db.query(Memo).all() # 모든 memo 다 가져오기
     return [{"id": memo.id, "title": memo.title, "content": memo.content} for memo in memos] #리스트 컴프리헨션으로 순회해서 리스트 추가
 
+#메모 수정
+@app.put('/memos/{memo_id}')
+async def create_memo(memo_id:int, memo:MemoUpdate, db:Session=Depends(get_db)):
+    db_memo = db.query(Memo).filter(Memo.id == memo_id).first() # 메모id가 같은 row만 필터링하고 그중 1개만 반환. findOne.
+    #애초에 메모 검색 안되면 에러임
+    if db_memo in None:
+        return ({"error": "Memo not found"})
+    
+    if memo.title is not None:
+        db_memo.title = memo.title
+
+    if memo.content is not None:
+        db_memo.content = memo.content
+
+    db.commit() #변경된 레코드를 바로 커밋
+    db.refresh(db_memo)
+    return ({"id": db_memo.id, "title": db_memo.title, "content": db_memo.content})
 
 @app.get('/')
 async def read_root(request: Request):
